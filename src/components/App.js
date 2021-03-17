38b7867c25d6145dfab3e3ac71b54af5d11c5484
Jason <jason.nordheim@gmail.com>
2021-03-17 01:39:26 -0600

    added Routing to Index component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import Navbar from "./layout/NavBar";
+import Index from "./layout/Index";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+      <div className="container">
+        <Switch>
+          <Route exact="/">
+            <Index />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
