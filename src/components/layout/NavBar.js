d5d2f5d36b58aa7b68132d513d1f0078b12de5ae
Jason <jason.nordheim@gmail.com>
2021-03-17 01:33:22 -0600

    created Navbar component

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand h1 mb-0 h1 mx-auto">Lyrical</span>
+    </nav>
+  );
+};
+
+export default App;
