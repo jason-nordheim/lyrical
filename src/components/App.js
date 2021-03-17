472a55d5814e17991c32c7cd64ee09a6c2ff6ce1
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-17 09:06:51 -0600

    wrapped App component with HOC Provider (Context)

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
 import Navbar from "./layout/NavBar";
 import Index from "./layout/Index";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "../context";
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact="/">
-            <Index />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route to="/" exact>
+              <Index />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
