b571d070241fb43c85412ff86adf7183e0a4968b
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-17 09:07:06 -0600

    added contet to Tracks Components (as conumer)

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { Consumer } from "../../context";
 const Tracks = () => {
+  const context = useContext(Consumer);
+  console.log(context);
   return <>Tracks Component</>;
 };
 
